13495aa9e2c3cf8cd4278b253b9677481a0fb50a
Om Goyal <omgoyal2711@gmail.com>
2025-07-16 17:34:04 +0530

    Add implementation of add.py for word input and saving to words.json

---
+++ add.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import json
+
+path = Path("words.json")
+words = json.loads(path.read_text(encoding='utf-8'))
+while True:                                                 # Adds words in the words.json file
+    new_word = input("Enter the word: ")
+    if new_word != "save":
+        if not new_word in words:
+            words.append(new_word)
+    else:
+        print("Saved!")
+        path.write_text(json.dumps(words))
